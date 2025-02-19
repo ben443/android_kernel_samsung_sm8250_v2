0e417babf9cc20e6dfea267fb8ad0e3cc3cd20ce
Ben graham <64079735+ben443@users.noreply.github.com>
2025-02-19 11:48:16 +1100

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,9 +6,9 @@ VARIANT=$1
 export ARCH=arm64
 mkdir out
 
-DTS_DIR=$(pwd)/out/arch/$ARCH/boot/dts
+DTS_DIR=$(pwd)/arch/$ARCH/boot/dts
 DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-*-r*.dtbo)
-rm ${DTS_DIR}/vendor/qcom/*.dtb ${DTBO_FILES}
+#rm ${DTS_DIR}/vendor/qcom/*.dtb ${DTBO_FILES}
 
 BUILD_CROSS_COMPILE=aarch64-linux-gnu-
 BUILD_CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
@@ -32,13 +32,13 @@ make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE
 make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE CROSS_COMPILE_COMPAT=$BUILD_CROSS_COMPILE_COMPAT LLVM=1 LLVM_IAS=1 CLANG_TRIPLE=$CLANG_TRIPLE $SAVE_DEFCONFIG || exit 1
 
 if [[ ! -z "$SAVE_DEFCONFIG" ]]; then
-cp $(pwd)/out/defconfig $(pwd)/arch/$ARCH/configs/vendor/x1q_chn_openx_defconfig
+cp $(pwd)/defconfig $(pwd)/arch/$ARCH/configs/vendor/x1q_chn_openx_defconfig
 exit 0
 fi
 
-cp $(pwd)/out/arch/$ARCH/boot/Image $(pwd)/out/Image
+cp $(pwd)/arch/$ARCH/boot/Image $(pwd)/Image
 
 DTBO_FILES=$(find ${DTS_DIR}/samsung/ -name ${CHIPSET_NAME}-sec-*-r*.dtbo)
 
-cat ${DTS_DIR}/vendor/qcom/*.dtb > $(pwd)/out/dtb.img
+cat ${DTS_DIR}/vendor/qcom/*.dtb > $(pwd)/dtb.img
 $(pwd)/tools/mkdtimg create $(pwd)/out/dtbo.img --page_size=4096 ${DTBO_FILES}
